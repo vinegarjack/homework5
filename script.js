695ca94c8075b6bf3e68c60d1dcf749f5648d7a9
Kataev Evgen <vinegarjack2012@gmail.com>
2019-11-13 00:00:53 +0200

    task3 completed

--- script.js
+++ script.js
@@ -6,8 +6,16 @@
         ],
         [
             [5,6],
-            [7,8,9],
+            [7,8,9,10],
         ]
     ];
-    
+    function sumArrElem(array){
+        let sum = 0;
+        array.forEach(element => {
+            element.forEach(innerElem => {
+                innerElem.forEach(muchInnerElement => sum += muchInnerElement)});
+        });
+        return sum;
+    }
+    console.log(sumArrElem(new3dArr));
 })();
