7653e47491d58e792fc04df23d3b6fc8c3054399
Kataev Evgen <vinegarjack2012@gmail.com>
2019-11-13 00:07:48 +0200

    Task4

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 (function (){
+    function isNumberInRange(num){
+        if (num > 0 && num <10){
+            return true;
+        }else {
+            return false;
+        }
+    }
 
+    console.log(isNumberInRange(1));
+    console.log(isNumberInRange(10));
 })();
