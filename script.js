cac5421b71b3aeaf468867738c1d871d02ee630c
Kataev Evgen <vinegarjack2012@gmail.com>
2019-11-12 23:47:00 +0200

    Task2

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 (function (){
-
+    const arr = [
+        [1,2,3],
+        [4,5],
+        [6,7,8,9],
+    ];
+    function sumArrElem(array){
+        let sum = 0;
+        array.forEach(element => {
+            element.forEach(innerElem => sum += innerElem);
+        });
+        return sum;
+    }
+    console.log(sumArrElem(arr));
 })();
