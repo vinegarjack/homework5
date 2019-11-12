bfa76ca96bcaf23c383ba8bc6ddf2e81d70a4a83
Kataev Evgen <vinegarjack2012@gmail.com>
2019-11-13 00:42:39 +0200

    Task5

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 (function (){
-
+    function isEven(num){
+        if (num%2===0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    console.log(isEven(23));
+    console.log(isEven(56));
 })();
