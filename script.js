993c92a27e03ed57fc7464fd233a4c146fabdfb8
Kataev Evgen <vinegarjack2012@gmail.com>
2019-11-12 23:31:48 +0200

    Task1

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 (function (){
-
+    "use strict";
+    function arrayFill(string, number){
+        const newArray=[];
+        for (let i = 0; i < number; i++) {
+           newArray.push(string);
+       } 
+    }
 })();
