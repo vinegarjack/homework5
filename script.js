11692909d940ff0dc0010ff4185fd97b0a1b058f
Kataev Evgen <vinegarjack2012@gmail.com>
2019-11-14 01:01:57 +0200

    task12

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 (function (){
-
+    function arrSize(array){
+        if (array === undefined){
+            console.log('error: no arguments');
+            return;
+        }else{
+            let size = 0;
+            array.forEach(element => size++);
+            return size;}
+        }
+    const arr = [1,3,5,6,8,23];
+    console.log(arrSize(arr));
+    console.log(arrSize([]));
+    arrSize();
 })();
