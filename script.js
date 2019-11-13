f27ce1f73433c7824ff2128014ff643cc0c969c4
Kataev Evgen <vinegarjack2012@gmail.com>
2019-11-13 03:15:00 +0200

    task7

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 (function (){
-
+    function getDivisors(number){
+        const divisors = [];
+        for (let i = 1; i <= number; i++) {
+            if (number%i===0){
+                divisors.push(i);
+            }
+        }
+        return divisors;
+    }
+    console.log(getDivisors(99));
 })();
