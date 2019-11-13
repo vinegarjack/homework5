7d4cf4bcc85b78c08eb1574946eb0e37c46c1707
Kataev Evgen <vinegarjack2012@gmail.com>
2019-11-14 00:29:53 +0200

    task10

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 (function (){
-
+    function yourAge(age){
+        let message ='';
+        if (age >16){
+            message = 'Welcome';
+        }else{
+            message = 'Too young';
+        };
+        return message;
+    }
+    console.log(yourAge(parseInt(prompt('Enter your age',''))));
 })();
