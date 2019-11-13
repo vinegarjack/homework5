227e1cb922a5570f7234219b52274a3080908727
Kataev Evgen <vinegarjack2012@gmail.com>
2019-11-14 01:12:45 +0200

    task13

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 (function (){
-
+    function someNumCheck(num){
+        if (num >= 10){
+            return num**2;
+        }else if (num < 7){
+            return 'Number lower than 7';
+        }else if(num === 8){
+            return 7;
+        }else if (num=== 9){
+            return 8;
+        }
+    }
+    console.log(someNumCheck(parseInt(prompt('enter number',''))));
 })();
