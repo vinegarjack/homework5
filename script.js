e0dfd93e2ac120ddf9214aab0e64311bc9ab686c
Kataev Evgen <vinegarjack2012@gmail.com>
2019-11-13 03:06:41 +0200

    task6

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 (function (){
-
+    function isEven(num){
+        if (num%2===0){
+            return true;
+        }else{
+            return false;
+        }
+    };
+    const array = [1,2,3,4,5,6,7,9,10];
+    const evenArray = array.filter(element => isEven(element));
+    console.log(array, evenArray);
 })();
