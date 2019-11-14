5912d5cfb2afb61cebc9f46e70a84d97ea84237b
Kataev Evgen <vinegarjack2012@gmail.com>
2019-11-14 03:06:17 +0200

    task17

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 (function (){
     "use strict";
-   
+    function change (array){
+        for (let i = 0; i < array.length; i+=2) {
+            [array[i], array[i+1]] = [array[i+1],array[i]]; 
+        };
+        return array;
+    }
+    const someArray = [1,2,3,4,5,6];
+    console.log(change(someArray));
 })();
