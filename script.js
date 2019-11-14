26e62651eb845c35886f0ae1c7ae5165ab5e2262
Kataev Evgen <vinegarjack2012@gmail.com>
2019-11-14 04:53:23 +0200

    task20

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 (function (){
     "use strict";
-   
+    function square (x){ 
+        return x*x;
+    }
+    function map( fn , array){
+        const inArr = [];
+        array.forEach(element => {
+            inArr.push( fn (element));
+        });
+        return inArr;
+    }
+    const anyArr = map(square,[1,2,3]);
+    console.log(anyArr);
 })();
