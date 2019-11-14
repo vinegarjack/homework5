13b2a6129e3eec3512e537f913716d5e42e58902
Kataev Evgen <vinegarjack2012@gmail.com>
2019-11-14 05:23:47 +0200

    task21

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 (function (){
     "use strict";
-   
+    function filter(fnc, array){
+        const innerArr = [];
+        array.forEach(element => {
+            if (fnc(element)){
+                innerArr.push(element);
+            }
+        });
+        return innerArr;
+    }
+    function isOdd(num){
+        if (num % 2 === 0 ){
+            return false;
+        }else{
+            return true;
+        }
+    }
+    const someArr = [1,2,3,4,56]
+    console.log(filter(isOdd, someArr));
 })();
