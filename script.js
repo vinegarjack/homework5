eb397e168f8873aa3311ef4daf75110c4e5b1108
Kataev Evgen <vinegarjack2012@gmail.com>
2019-11-14 02:52:02 +0200

    task16

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 (function (){
     "use strict";
-   
+    function inArray(searchString, stringArray){
+        for (let i = 0; i < stringArray.length; i++) {
+            if (stringArray[i].includes(searchString)){
+                return true;
+            }
+        };
+        return false;
+    }
+    const array = [
+        'good',
+        'day',
+        'to',
+        'die',
+    ]
+    console.log(inArray('day', array));
 })();
