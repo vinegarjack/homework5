69613e2c5ea2f78ef22680a30d3caa12f9322c19
Kataev Evgen <vinegarjack2012@gmail.com>
2019-11-14 02:01:22 +0200

    task14

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 (function (){
-
+    function ucfirst(string){
+        let argString = string.trim(), resString='';
+        resString = string[0].toUpperCase();
+        for (let i = 1; i < argString.length; i++) {
+            if (argString[i] !== ' '){
+                resString += argString[i];
+            }else{
+                resString += ' ' + argString[++i].toUpperCase();
+            }
+        }
+        return resString;
+    }
+    let someString = 'abra kadabra boom';
+    console.log(ucfirst(someString));
 })();
