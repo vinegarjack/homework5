7f31781f357787ddcb0c62bb07be09f9a123fb7c
Kataev Evgen <vinegarjack2012@gmail.com>
2019-11-14 02:22:55 +0200

    task15

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 (function (){
-
+    "use strict";
+    function someUpperCase(string){
+        const stringArr = string.split('_');
+        let resString = stringArr[0];
+        for (let i = 1; i < stringArr.length; i++) {
+            resString += stringArr[i][0].toUpperCase() + stringArr[i].slice(1);
+        };
+        return resString;
+    }
+    const someString = 'var_text_hello';
+    console.log(someUpperCase(someString));
 })();
