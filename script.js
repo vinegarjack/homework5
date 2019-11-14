44fc869a8d9096616ae3da8aeea7339927388949
Kataev Evgen <vinegarjack2012@gmail.com>
2019-11-14 04:37:24 +0200

    task19

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 (function (){
     "use strict";
-   
+    function sequence(start = 0, step = 1){
+        let startIn = start, stepIn = step;
+        return function() {
+            return startIn += stepIn; 
+        };
+    }
+    let gen = sequence(3,4);
+    function take (num){
+        const array =[];
+        for(let i = 0; i < num ; i++){
+            array.push(gen());
+        }
+        return array;
+    }
+    console.log(take(5));
 })();
