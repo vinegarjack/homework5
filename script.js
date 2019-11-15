252132293c8679a4c65469b439c3572164fd3ced
Kataev Evgen <vinegarjack2012@gmail.com>
2019-11-15 18:59:24 +0200

    task20spec

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 (function (){
     "use strict";
-   
+    function sequance (start = 0, step = 1){
+        return function(){
+            return start +=step;
+        }
+    }
+    let gen = sequance();
+    /* console.log(gen());
+    console.log(gen()); */
+    function fmap(fun1, fun2){
+        
+    }
 })();
