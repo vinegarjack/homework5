30dd1a06f0bda891c0e67ed7aef219a8f87a3419
Kataev Evgen <vinegarjack2012@gmail.com>
2019-11-20 21:52:10 +0200

    try

--- script.js
+++ script.js
@@ -5,10 +5,23 @@
             return start +=step;
         }
     }
+
     let gen = sequance();
+    function add(){
+        let summ = 0;
+        for (let i = 0; i < arguments.length; i++) {
+            summ += arguments[i];
+        }
+        return summ;
+    }
+    function square(num){
+        return num **2;
+    }
     /* console.log(gen());
     console.log(gen()); */
     function fmap(fun1, fun2){
-        
+        const firstFun = fun1(), secondFun = fun2();
+        return firstFun(secondFun());
     }
+    console.log(fmap(add(1,2,3),square()));
 })();
